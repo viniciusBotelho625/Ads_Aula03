64dc3aaf918c68df635d48948f8e9c9b33260252
Yuri Ramalho <yuri.ramalho.silva.16@gmail.com>
2020-03-28 22:49:37 -0300

    criação de classe poligono + atributos e construtor

---
+++ src/Circulo.java
@@ -0,0 +1,10 @@
+public class Circulo extends Figura{
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
---
+++ src/Poligono.java
@@ -0,0 +1,17 @@
+
+public class Poligono extends Figura {
+	private double base;
+	private double altura;
+	
+	public Poligono(double base, double altura) {
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
